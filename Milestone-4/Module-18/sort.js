1cb84c0428681dba9495bbfd69eb753093fea073
abin0x <mahmudulabin@gmail.com>
2025-08-25 10:36:06 +0600

    Ascending and descending order

--- Milestone-4/Module-18/sort.js
+++ Milestone-4/Module-18/sort.js
@@ -1,3 +1,13 @@
 const numbers = [5, 3, 8, 1, 2];
 numbers.sort();
 console.log(numbers);
+
+const fruits = ['banana', 'apple', 'cherry', 'date'];
+fruits.sort();
+console.log(fruits);
+const ages=[1,5,2,88,77,3,4,6,22,55];
+const age_sorted=ages.sort(function(a,b){return a-b});
+console.log(age_sorted);
+const ages_des=[1,5,2,88,77,3,4,6,22,55];
+const age_sorted_descending=ages_des.sort(function(a,b){return b-a});
+console.log(age_sorted_descending);
