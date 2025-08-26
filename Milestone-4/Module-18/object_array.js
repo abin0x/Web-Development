c86fc433138cbf65bd5578e04738944d86fd5281
abin0x <mahmudulabin@gmail.com>
2025-08-26 09:53:58 +0600

    Object array

---
+++ Milestone-4/Module-18/object_array.js
@@ -0,0 +1,7 @@
+const employees=[
+    {name:"mahmudul hasan abin",age:22,designation:"Junior developer",salary:30000},
+    {name:"John Doe",age:30,designation:"Senior Developer",salary:60000},
+    {name:"Jane Smith",age:28,designation:"Project Manager",salary:80000},
+    {name:"Alice Johnson",age:35,designation:"UX Designer",salary:70000}
+]
+console.log(employees[0]);
