a247e277d565988c12188e97d19e04b17b0d8adb
abin0x <mahmudulabin@gmail.com>
2025-08-27 20:02:08 +0600

    Function learning

---
+++ Milestone-4/Module-19/function.js
@@ -0,0 +1,16 @@
+const price=50;
+
+function daatBrushKor()
+{
+    console.log("Daat brush kor");
+    console.log("Price: " + price);
+    console.log("Daat brush kor");
+}
+function fanOnKor()
+{
+    console.log("Fan on kor");
+    console.log("Price: " + price);
+    console.log("Fan on kor");
+}
+daatBrushKor();
+fanOnKor();
