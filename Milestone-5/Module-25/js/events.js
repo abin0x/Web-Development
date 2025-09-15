f9acde67b86919a034b92ed66d0401acd67475ec
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-09-15 13:23:55 +0600

    now here add there are 3 types of events in button

--- Milestone-5/Module-25/js/events.js
+++ Milestone-5/Module-25/js/events.js
@@ -1,1 +1,25 @@
 console.log("external js/events.js loaded");
+// alert("External JS file loaded");
+function makeYellow()
+{
+    document.body.style.backgroundColor = 'yellow';
+}
+function makeBlue()
+{
+    document.body.style.backgroundColor = 'blue';
+}
+function makePink()
+{
+    document.body.style.backgroundColor = 'pink';
+}
+const btnMakeBlack=document.getElementById('btn-make-black');
+// console.log(btnMakeBlack);
+btnMakeBlack.onclick=function makeBlack(){
+    document.body.style.backgroundColor='black';
+}
+const btnMakeGreen=document.getElementById('btn-make-green');
+        btnMakeGreen.onclick=makeGreen;
+        function makeGreen()
+        {
+            document.body.style.backgroundColor='green';
+        }
