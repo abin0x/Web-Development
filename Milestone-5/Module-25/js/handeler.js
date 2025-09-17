74d697b56f6aa370585167cba1ca8baaf45c7f40
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-09-17 11:28:03 +0600

    Handler learning

---
+++ Milestone-5/Module-25/js/handeler.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-update-title')
+        .addEventListener('click',function(){
+            const pageTitle=document.getElementById('page-title');
+            pageTitle.innerText = 'Title Updated';
+        })
