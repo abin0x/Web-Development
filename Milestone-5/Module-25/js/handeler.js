f8747750d7ff41bbfa33ed96dfd0a0835338ee9b
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-09-17 11:32:58 +0600

    User login tiitle change in eventlisener

--- Milestone-5/Module-25/js/handeler.js
+++ Milestone-5/Module-25/js/handeler.js
@@ -3,3 +3,9 @@ document.getElementById('btn-update-title')
             const pageTitle=document.getElementById('page-title');
             pageTitle.innerText = 'Title Updated';
         })
+
+document.getElementById('btn-login')
+        .addEventListener('click',function(){
+            const userLogin=document.getElementById('user-status');
+            userLogin.innerText='User Logged In';
+        })
